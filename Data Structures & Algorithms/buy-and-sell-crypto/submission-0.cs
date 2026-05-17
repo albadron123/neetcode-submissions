f66fe9331a6991edb8e45e8484e56f6d96295acd
Maxim Pchelintsev <60190751+albadron123@users.noreply.github.com>
2026-05-17 11:30:55 +0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int maxProfit = 0;
+        if(prices.Length == 1) return maxProfit;
+        int minPrice = Int32.MaxValue;
+        for(int sellPrice = 0; sellPrice < prices.Length; ++sellPrice)
+        {
+            if(prices[sellPrice] < minPrice) 
+                minPrice = prices[sellPrice];
+            if(prices[sellPrice] - minPrice > maxProfit) 
+                maxProfit = prices[sellPrice] - minPrice;
+        }
+        return maxProfit;
+    }
+}
